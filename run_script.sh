d07a6c996ace1d7719c24ff7c10f69983e1ef5a2
Crosby <schcrosby@gmail.com>
2017-11-15 09:16:49 -0800

    asdfas

--- run_script.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-PATH=/home/crosby/bin:/home/crosby/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin
-
-
-cd /home/crosby/Documents/SkagitOperational
-python Run_model.py > 'skagit.log'
